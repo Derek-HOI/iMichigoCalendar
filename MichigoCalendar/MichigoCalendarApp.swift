215257ccb428c9e7f54e62b52737fa5f236c84bb
YangjinByun <byj0515@naver.com>
2025-01-01 17:06:05 +0900

    Initial Commit

---
+++ MichigoCalendar/MichigoCalendarApp.swift
@@ -0,0 +1,32 @@
+//
+//  MichigoCalendarApp.swift
+//  MichigoCalendar
+//
+//  Created by 변양진 on 1/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MichigoCalendarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MichigoCalendarTests/MichigoCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  MichigoCalendarTests.swift
+//  MichigoCalendarTests
+//
+//  Created by 변양진 on 1/1/25.
+//
+
+import Testing
+@testable import MichigoCalendar
+
+struct MichigoCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
